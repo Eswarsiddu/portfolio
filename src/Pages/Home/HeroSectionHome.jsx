c969109c6b32d620b8f95bed3398c58c54027d98
Siddu Eswar <seswar@egineet.com>
2024-07-18 13:29:33 -0400

    Clean Herosection code

--- src/Pages/Home/HeroSectionHome.jsx
+++ src/Pages/Home/HeroSectionHome.jsx
@@ -1,58 +1,12 @@
-import { useEffect, useRef, useState } from "react";
-import BG1 from "/images/svg.png";
-// import BG2 from "/images/svg(1).png";
 import Lottie from "lottie-react";
-// import GIF from "/9xev5OKerr.gif";
-// import GIF from "../../Assets/9xev5OKerr.json";
 import GIF from "../../Assets/2TubWAO9Je.json";
-import Typed from "typed.js";
-import Packman from "/images/pacman.svg";
 
 function HeroSection({ NavBarHeight }) {
-  // const [HeroSectionHeight, setHeroSectionHeight] = useState(
-  //   window.innerHeight - NavBarHeight
-  // );
-
-  // useEffect(() => {
-
-  // }, []);
   const HeroSectionHeight = window.innerHeight - NavBarHeight;
-  const typedRef = useRef(null);
-
-  useEffect(() => {
-    // setTimeout(() => {
-    //   setHeroSectionHeight(window.innerHeight - NavBarHeight);
-    // }, 500);
-    // window.addEventListener("resize", () => {
-    //   setHeroSectionHeight(window.innerHeight - NavBarHeight);
-    // });
-    // if (!typedRef.current) {
-    //   return () => {
-    //     window.removeEventListener("resize", () => {
-    //       setHeroSectionHeight(window.innerHeight - NavBarHeight);
-    //     });
-    //   };
-    // }
-    if (!typedRef.current) return;
-    const typed = new Typed(typedRef.current, {
-      strings: ["A Web^800", "An App^500"],
-      typeSpeed: 50,
-      backSpeed: 20,
-      // waitTime: 800,
-      loop: true,
-      cursorChar: "|",
-    });
-    return () => {
-      typed.destroy();
-      // window.removeEventListener("resize", () => {
-      //   setHeroSectionHeight(window.innerHeight - NavBarHeight);
-      // });
-    };
-  }, []);
 
   return (
     <section
-      className=" container mx-auto text-white relative animate-bgChang"
+      className=" container mx-auto relative animate-bgChang"
       style={{
         height: `${HeroSectionHeight}px`,
       }}
@@ -81,40 +35,22 @@ function HeroSection({ NavBarHeight }) {
           </div>
 
           <div className="flex gap-4">
-            <button className="py-2 px-4 bg-[#7932d9] rounded-md group text-white hover:bg-[#6925c2] font-mediu font-bold relative">
+            <button className="py-2 px-4 bg-[#7932d9] rounded-md group hover:bg-[#6925c2] font-mediu font-bold relative">
               <span>Contact Me</span>
               <span className="block absolute w-full h-full rounded-md left-0 top-0 group-hover:left-1 group-hover:top-1 -z-10 transition-all duration-100 ease-in group-hover:bg-[#00f1ff]"></span>
             </button>
-            {/* <button className="p-2 bg-red-500 rounded text-white hover:bg-red-600 font-medium">
-              Resume
-            </button> */}
           </div>
         </div>
         <Lottie
           animationData={GIF}
           className="w-3/5 hidden md:block lg:w-2/5 bg-green-3"
-          style={
-            {
-              // width: "50%",
-              // height: "600px",
-              // alignSelf: "center",
-              // transform: "translateY(-25%)",
-            }
-          }
         />
-        {/* <img src={GIF} alt="" className="w-full" /> */}
-        {/* </div> */}
-        {/* <div className="w-1/2"> */}
-        {/* <img className=" w-[100px] h-[150px] " src="./images/eswar.jpg" /> */}
-        {/* </div> */}
-        {/* </div> */}
       </div>
       <div className="absolute bottom-16 left-1/2 -translate-x-1/2 opacity-20">
         {/* Scrool down to see more */}
         See What We Can Do
       </div>
       <button
-        // href={"#about-me-section"}
         className=" bg-[#7932d9] w-12 h-12 rounded-full absolute bottom-3 left-1/2 -translate-x-1/2 flex justify-center items-center "
         onClick={() => {
           window.scrollTo({
@@ -123,9 +59,7 @@ function HeroSection({ NavBarHeight }) {
           });
         }}
       >
-        {/* <img src={AnglesDown} /> */}
         <i className="fa-solid fa-angles-down mt-3 text-white text-2xl animate-bounce"></i>
-        {/* <ChevronsDown fill="#ffffff" stroke="1" /> */}
       </button>
     </section>
   );
